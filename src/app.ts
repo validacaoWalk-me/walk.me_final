69c2e4d806b18e877435839b9b0a04809f9f7653
gusta <gustavo.cesariosilva15@gmail.com>
2024-09-27 10:59:52 -0300

    Adicionando cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import routes from './routes/routes';
 import bodyParser from 'body-parser';
+import cors, { CorsOptions } from 'cors';
 
 const app = express();
+
+const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
